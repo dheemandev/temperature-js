f4e260cdbbcebe6b9ee98acc677b5d3d8a0fda02
dheemandev <dheemanchakma04@gmail.com>
2022-03-05 16:53:23 +0600

    temperature

---
+++ temperature-hot-master/app.js
@@ -0,0 +1,24 @@
+const API_KEY = `1e85ea722080a9279731254d0930b019
+`;
+
+const searchTemperature = () => {
+  const city = document.getElementById ('city-name').value 
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+  // console.log(url);
+  fetch(url)
+  .then(res => res.json())
+  .then( data => displayTemparature(data));
+
+}
+
+const innerText = (id,text) =>{
+   document.getElementById(id).innerText = text;
+
+
+}
+const displayTemparature = temparature => {
+  innerText ('city', temparature.name);
+  innerText ('tempt' , temparature.main.temp);
+  innerText ('condition', temparature.weather[0].main)
+  console.log(temparature);
+}
